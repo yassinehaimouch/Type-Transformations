66290b87aacbad05cf6943db2ca24cee587bfedc
yassinehaimouch <haymouch53@gmail.com>
2023-06-27 14:29:56 +0100

    Ex11 solution

--- src/02-unions-and-indexing/11-indexed-access-with-unions.problem.ts
+++ src/02-unions-and-indexing/11-indexed-access-with-unions.problem.ts
@@ -9,11 +9,10 @@ export const programModeEnumMap = {
   PLANNED_SELF_DIRECTED: "plannedSelfDirected",
 } as const;
 
-export type IndividualProgram = typeof programModeEnumMap[
-  | "ONE_ON_ONE"
-  | "SELF_DIRECTED"
-  | "PLANNED_ONE_ON_ONE"
-  | "PLANNED_SELF_DIRECTED"];
+export type IndividualProgram = typeof programModeEnumMap[Exclude<
+  keyof typeof programModeEnumMap,
+  "GROUP" | "ANNOUNCEMENT"
+>];
 
 type tests = [
   Expect<
