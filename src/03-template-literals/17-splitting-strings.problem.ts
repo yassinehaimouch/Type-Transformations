f9350b62ddecef33def995098165eb230f0fb8e3
yassinehaimouch <haymouch53@gmail.com>
2023-07-08 14:37:39 +0100

    Ex15 solution

--- src/03-template-literals/17-splitting-strings.problem.ts
+++ src/03-template-literals/17-splitting-strings.problem.ts
@@ -1,12 +1,9 @@
-// Might come in handy!
-// import { S } from "ts-toolbelt";
-// https://millsp.github.io/ts-toolbelt/modules/string_split.html
-
+import { S } from "ts-toolbelt";
 import { Equal, Expect } from "../helpers/type-utils";
 
 type Path = "Users/John/Documents/notes.txt";
 
-type SplitPath = unknown;
+type SplitPath = S.Split<Path, "/">;
 
 type tests = [
   Expect<Equal<SplitPath, ["Users", "John", "Documents", "notes.txt"]>>,
