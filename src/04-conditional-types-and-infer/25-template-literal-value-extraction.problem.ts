e018838bfd2735a3f5a0dc6b001ae1540db176a3
yassinehaimouch <haymouch53@gmail.com>
2023-07-27 15:01:34 +0100

    Ex24 solution

--- src/04-conditional-types-and-infer/25-template-literal-value-extraction.problem.ts
+++ src/04-conditional-types-and-infer/25-template-literal-value-extraction.problem.ts
@@ -8,7 +8,7 @@ type Names = [
   "BB King",
 ];
 
-type GetSurname<T> = unknown;
+type GetSurname<T> = T extends `${infer First} ${infer Last}` ? Last : never;
 
 type tests = [
   Expect<Equal<GetSurname<Names[0]>, "Pocock">>,
