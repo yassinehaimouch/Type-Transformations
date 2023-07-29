4b8e73dc6d9c3ed28062b84e7de1fe3b858099f7
yassinehaimouch <haymouch53@gmail.com>
2023-07-29 20:17:01 +0100

    Ex26 solution

--- src/04-conditional-types-and-infer/26-get-result-from-async-function.problem.ts
+++ src/04-conditional-types-and-infer/26-get-result-from-async-function.problem.ts
@@ -10,7 +10,11 @@ const getServerSideProps = async () => {
   };
 };
 
-type InferPropsFromServerSideFunction = unknown;
+type InferPropsFromServerSideFunction<T> = T extends () => Promise<{
+  props: infer P;
+}>
+  ? P
+  : never;
 
 type tests = [
   Expect<
