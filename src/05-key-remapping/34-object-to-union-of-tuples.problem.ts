9508732c7b5df4ad50d616632bce633b2bf96629
yassinehaimouch <haymouch53@gmail.com>
2023-08-09 12:19:25 +0100

    Ex34 solution

--- src/05-key-remapping/34-object-to-union-of-tuples.problem.ts
+++ src/05-key-remapping/34-object-to-union-of-tuples.problem.ts
@@ -7,8 +7,8 @@ interface Values {
 }
 
 type ValuesAsUnionOfTuples = {
-  [K in keyof Values]: [K, Values[K]];
-};
+  [V in keyof Values]: [V, Values[V]];
+}[keyof Values];
 
 type tests = [
   Expect<
@@ -16,5 +16,5 @@ type tests = [
       ValuesAsUnionOfTuples,
       ["email", string] | ["firstName", string] | ["lastName", string]
     >
-  >
+  >,
 ];
